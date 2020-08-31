020fe6cc6b85b4b261954a57c98e3bb93fe63724
Dark Sun <dacom.dark.sun@gmail.com>
2020-08-31 18:23:09 +0500

    initialization

---
+++ partners.cpp
@@ -0,0 +1,133 @@
+#include "partners.hpp"
+
+using namespace eosio;
+
+    /**
+   * @brief      Метод обновления профиля
+   * Операция обновления профиля позволяет изменить мета-данные аккаунта. 
+   * @param[in]  op    The operation
+   */
+  [[eosio::action]] void part::profupdate(eosio::name username, std::string meta){
+    require_auth(username);
+    partners_index refs(_me, _me.value);
+
+    auto ref = refs.find(username.value);
+    
+    refs.modify(ref, username, [&](auto &r){
+        r.meta = meta;
+    });
+  }
+
+
+/**
+ * @brief      Регистрация пользователя в системе. 
+ * Характеризуется созданием профиля с ссылкой на приглашающий аккаунт. Приглашающий аккаунт используется в качестве связи для вычисления партнерских структур различного профиля.
+ * 
+ * TODO ввести порядковые номера  
+ *
+ * @param[in]  op    The operation
+ */
+
+[[eosio::action]] void part::reg(eosio::name username, eosio::name referer, std::string meta) {
+    
+    eosio::check(has_auth(username) || has_auth(part::_me), "missing required authority");
+
+    eosio::check( is_account( username ), "User account does not exist");
+    
+     
+    partners_index refs(_me, _me.value);
+    auto ref = refs.find(username.value);
+
+    eosio::check(username != referer, "You cant set the referer yourself");
+    
+    if (!has_auth(part::_me)){
+        eosio::check(referer.value != 0, "Registration without referer is not possible");
+        eosio::check( is_account( referer ), "Referer account does not exist");
+        auto pref = refs.find(referer.value);
+        eosio::check(pref != refs.end(), "Referer is not registered in the core");    
+    } 
+
+    
+    userscount_index usercounts(_me, _me.value);
+    auto usercount = usercounts.find("registered"_n.value);
+    uint64_t count = 1;
+
+
+    //TODO eosio::check account registration;
+    
+    if (ref == refs.end()){
+        if (usercount == usercounts.end()){
+            usercounts.emplace(_me, [&](auto &u){
+                u.cname = "registered"_n;
+                u.count = count;
+            });
+        } else {
+            count =  usercount -> count + 1;
+            usercounts.modify(usercount, _me, [&](auto &u){
+                u.count = count;
+            });
+        };
+
+        refs.emplace(_me, [&](auto &r){
+            r.id = count;
+            r.username = username;
+            r.referer = referer;
+            r.meta = meta;
+        });
+    } else {
+        require_auth(part::_me); //only registrator can change referer
+
+        refs.modify(ref, _me, [&](auto &r){
+            r.referer = referer;
+        });
+    }
+};
+
+
+[[eosio::action]] void part::del(eosio::name username){
+    require_auth(part::_me);
+    partners_index refs(_me, _me.value);
+    auto u = refs.find(username.value);
+    refs.erase(u);
+    userscount_index usercounts(_me, _me.value);
+    auto usercount = usercounts.find("registered"_n.value);
+    usercounts.modify(usercount, _me, [&](auto &u){
+        u.count = u.count - 1;
+    });
+
+}
+
+
+
+
+extern "C" {
+   
+   /// The apply method implements the dispatch of events to this contract
+   void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
+        if (code == part::_me.value) {
+          if (action == "reg"_n.value){
+            execute_action(name(receiver), name(code), &part::reg);
+          }  else if (action == "del"_n.value){
+            execute_action(name(receiver), name(code), &part::del);
+          }  else if (action == "profupdate"_n.value){
+            execute_action(name(receiver), name(code), &part::profupdate);
+          }  
+        } else {
+          if (action == "transfer"_n.value){
+            
+            struct transfer{
+                eosio::name from;
+                eosio::name to;
+                eosio::asset quantity;
+                std::string memo;
+            };
+
+            auto op = eosio::unpack_action_data<transfer>();
+
+            if (op.to == part::_me){
+              //Do the stuff
+            }
+          }
+        }
+  };
+};
---
+++ partners.hpp
@@ -0,0 +1,79 @@
+#include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+#include <eosio/system.hpp>
+#include <eosio/time.hpp>
+#include <eosio/multi_index.hpp>
+#include <eosio/contract.hpp>
+#include <eosio/crypto.hpp>
+
+// #define isDebug = TRUE
+class [[eosio::contract]] part : public eosio::contract {
+
+public:
+    part( eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds ): eosio::contract(receiver, code, ds)
+    {}
+
+    [[eosio::action]] void reg(eosio::name username, eosio::name referer, std::string meta);
+    [[eosio::action]] void del(eosio::name username);
+    [[eosio::action]] void profupdate(eosio::name username, std::string meta);
+
+
+    void apply(uint64_t receiver, uint64_t code, uint64_t action);
+    
+    
+    static constexpr eosio::name _me = "part"_n;
+        
+    
+    static uint128_t combine_ids(const uint64_t &x, const uint64_t &y) {
+        return (uint128_t{x} << 64) | y;
+    };
+
+
+     struct [[eosio::table]] userscount
+     {
+         eosio::name cname = "registered"_n;
+         uint64_t count;
+         
+         uint64_t primary_key() const {return cname.value;}
+
+         EOSLIB_SERIALIZE(userscount, (cname)(count))
+     }; 
+
+    typedef eosio::multi_index<"userscount"_n, userscount> userscount_index;
+
+
+    struct [[eosio::table]] partners {
+        eosio::name username;
+        eosio::name referer;
+        std::string nickname;
+        eosio::checksum256 nickhash;
+        
+        uint64_t id;
+        std::string meta;
+        
+        uint64_t primary_key() const{return username.value;}
+        uint64_t byreferer() const{return referer.value;}
+        uint64_t byid() const {return id;}
+
+        eosio::checksum256 bynickhash() const { return nickhash; }
+        
+        EOSLIB_SERIALIZE(partners, (username)(referer)(id)(meta)(nickhash))
+
+
+    };
+      
+      eosio::checksum256 hashit(std::string str) const
+        {
+            return eosio::sha256(const_cast<char*>(str.c_str()), str.size());
+        }
+        
+        
+    typedef eosio::multi_index<"partners"_n, partners,
+      eosio::indexed_by<"byreferer"_n, eosio::const_mem_fun<partners, uint64_t, &partners::byreferer>>,
+      eosio::indexed_by<"byid"_n, eosio::const_mem_fun<partners, uint64_t, &partners::byid>>,
+      eosio::indexed_by<"bynickhash"_n, eosio::const_mem_fun<partners, eosio::checksum256, &partners::bynickhash>>
+    > partners_index;
+
+
+
+};
